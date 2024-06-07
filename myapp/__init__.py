66cd6cb694b521ad3c80b2e4ab7d20e6c672aec1
Jash Sureja <sureja.jash@gmail.com>
2024-06-07 19:37:44 +0530

    dropbox uploading done, retrieving remaining

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    dropbox_token = os.getenv("DROPBOX_TOKEN")
+    app.config["DROPBOX_ACCESS_TOKEN"] = os.getenv("DROPBOX_TOKEN")
     db.init_app(app)
 
     app.register_blueprint(main)
--- myapp/routes.py
+++ myapp/routes.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, redirect, url_for, render_template
+import io
+import os
+
+import dropbox
+from flask import Blueprint, redirect, render_template, request, url_for
 
 from .extensions import db
 from .models import User
+
+access_token = os.getenv("DROPBOX_TOKEN")
+refresh_token = os.getenv("REFRESH_TOKEN")
+app_key = os.getenv("APP_KEY")
+app_secret = os.getenv("REFRESH_TOKEN")
+
+
 
 main = Blueprint('main', __name__)
 
@@ -10,9 +21,35 @@ def index():
     users = User.query.all()
     users_list_html = [f"<li>{ user.username }</li>" for user in users]
     f"<ul>{''.join(users_list_html)}</ul>"
+    
     return render_template('upload.html')
 
-
+@main.route('/', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        dbx = dropbox.Dropbox(
+            oauth2_access_token=access_token,
+            app_key = app_key,
+            app_secret = app_secret
+            # oauth2_refresh_token = ""
+        )
+        filename = file.filename
+        file_stream = io.BytesIO(file.read())
+        dropbox_destination = "/"
+        try:
+            dbx.files_upload(file_stream.getvalue(), f'/{filename}')
+            shared_link_metadata = dbx.sharing_create_shared_link(dropbox_destination)
+            
+            url = shared_link_metadata
+            print( url)
+            return 'File uploaded successfully'
+        except dropbox.exceptions.ApiError as err:
+            return f'Failed to upload file: {err}'
 
 
 @main.route('/add/<username>')
